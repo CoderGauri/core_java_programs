718bfc2233926083e619471ef66a5bf20452f046
Gauri <gauriM304@gmail.com>
2025-05-07 22:04:51 +0530

    basic calculator program

---
+++ src/Calculator.java
@@ -0,0 +1,9 @@
+public class Calculator {
+    public static void main(String[] args) {
+        int res = add(3,4);
+        System.out.println(res);
+    }
+    public static int add(int a , int b){
+        return a +b;
+    }
+}
