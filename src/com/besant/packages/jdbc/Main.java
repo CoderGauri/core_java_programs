0d75d1ac14bf3fed218f51b99bc2de483f5b9f4f
Gauri <gauriM304@gmail.com>
2025-05-26 09:45:08 +0530

    JDBC

---
+++ src/com/besant/packages/jdbc/Main.java
@@ -0,0 +1,13 @@
+package com.besant.packages.jdbc;
+
+public class Main {
+    public static void main(String[] args) {
+        //steps to connect java to DB
+        //create a driver
+        //create a connection --auth
+        //preapring the query
+        //excecute the query
+        //close the connections
+
+    }
+}
---
+++ src/com/besant/packages/oops/abstraction/Animals.java
@@ -0,0 +1,25 @@
+//package com.besant.packages.oops.abstraction;
+//
+//public static void main(String[] args) {
+//
+//  // Abstract method (does not have a body)
+//    abstract void makeSound();
+//
+//    // Regular method
+//    void sleep() {
+//        System.out.println("Sleeping...");
+//    }
+//}
+//
+//// Subclass (inherits from Animal)
+//class Dog extends Animals {
+//    // Providing implementation of abstract method
+//    void makeSound() {
+//        System.out.println("Bark");
+//    }
+//}
+//
+//
+//
+//
+//
