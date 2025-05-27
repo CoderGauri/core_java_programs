b3140b248a82dafe04c450043d901896b8ea6188
Gauri <gauriM304@gmail.com>
2025-05-27 09:12:10 +0530

    JDBC

--- src/com/besant/packages/jdbc/Main.java
+++ src/com/besant/packages/jdbc/Main.java
@@ -1,5 +1,7 @@
 package com.besant.packages.jdbc;
 
+import java.sql.*;
+
 public class Main {
     public static void main(String[] args) {
         //steps to connect java to DB
@@ -8,6 +10,25 @@ public class Main {
         //preapring the query
         //excecute the query
         //close the connections
-
+        PreparedStatement statement Connection connection;
+        try {
+            //creating a driver
+            Class.forName("com.mysql.cj.jsbc.Driver");
+            //create a connection
+            connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/jdbc", "root", "Gauri@1029");
+//step 3
+            //prepare the query
+            statement = connection.prepareStatement("insery into user (name) values ('nmae1')");
+        } catch (ClassNotFoundException e) {
+            throw new RuntimeException(e);
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+//step 4
+//excecute the query
+        statement.executeUpdate();
+catch(Exception e)
+        //TODO : handle exception
+        connection.close();
     }
 }
